beb1b5bf313a901bdfc1a05cd2bf2626f583a624
Christof Ruch <christof.ruch@gmx.de>
2021-04-05 21:35:08 +0200

    Fixing Linux build

--- Category.cpp
+++ Category.cpp
@@ -10,7 +10,7 @@
 
 namespace midikraft {
 
-	bool midikraft::operator==(Category const &left, Category const &right)
+	bool operator==(Category const &left, Category const &right)
 	{
 		// Ignore bitIndex and color
 		return left.def_->id == right.def_->id;
@@ -21,33 +21,33 @@ namespace midikraft {
 		return left.def_->id < right.def_->id;
 	}
 
-	std::set<midikraft::Category> midikraft::category_union(std::set<Category> const &a, std::set<Category> const &b)
+	std::set<midikraft::Category> category_union(std::set<Category> const &a, std::set<Category> const &b)
 	{
 		std::set<Category> result;
 		std::set_union(a.cbegin(), a.cend(), b.cbegin(), b.cend(), std::inserter(result, result.begin()));
 		return result;
 	}
 
-	std::set<midikraft::Category> midikraft::category_intersection(std::set<Category> const &a, std::set<Category> const &b)
+	std::set<midikraft::Category> category_intersection(std::set<Category> const &a, std::set<Category> const &b)
 	{
 		std::set<Category> result;
 		std::set_intersection(a.cbegin(), a.cend(), b.cbegin(), b.cend(), std::inserter(result, result.begin()));
 		return result;
 	}
 
-	std::set<midikraft::Category> midikraft::category_difference(std::set<Category> const &a, std::set<Category> const &b)
+	std::set<midikraft::Category> category_difference(std::set<Category> const &a, std::set<Category> const &b)
 	{
 		std::set<Category> result;
 		std::set_difference(a.cbegin(), a.cend(), b.cbegin(), b.cend(), std::inserter(result, result.begin()));
 		return result;
 	}
 
-	std::string midikraft::Category::category() const
+	std::string Category::category() const
 	{
 		return def_->name;
 	}
 
-	juce::Colour midikraft::Category::color() const
+	juce::Colour Category::color() const
 	{
 		return def_->color;
 	}
