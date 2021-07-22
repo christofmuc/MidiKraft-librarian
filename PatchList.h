001b3caa0e4665598fe92620fa3fb2ba3cb74af1
Christof Ruch <christof.ruch@gmx.de>
2021-07-22 23:49:24 +0200

    Adding the concept of a PatchList aka user list of patches that can be used in the software to organize patches, e.g. some that fit well together, or some that are candidates for a specific song.

---
+++ PatchList.h
@@ -0,0 +1,32 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
+#pragma once
+
+#include "PatchHolder.h"
+
+namespace midikraft {
+
+	class PatchList {
+	public:
+		PatchList(std::string const& name);
+		PatchList(std::string const& id, std::string const &name);
+		
+		std::string name() const;
+		void setName(std::string const& new_name);
+
+		void setPatches(std::vector<PatchHolder> patches);
+		std::vector<PatchHolder> patches() const;
+		void addPatch(PatchHolder patch);
+		
+
+	private:
+		std::string id_;
+		std::string name_;
+		std::vector<PatchHolder> patches_;
+	};
+
+}
